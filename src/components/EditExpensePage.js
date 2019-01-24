dcba76364537795392db7c488e3ee56160dcd2cc
Michael Evans <azmathtech@gmail.com>
2019-01-24 13:13:45 -0700

    Setup startRemoveExpense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,33 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import ExpenseForm from './ExpenseForm';
-import { editExpense, removeExpense } from '../actions/expenses';
+import { editExpense, startRemoveExpense } from '../actions/expenses';
 
-const EditExpensePage = props => {
-  console.log(props);
-  return (
-    <div>
-      Editing the expense with id of {props.match.params.id}
-      <ExpenseForm
-        expense={props.expense}
-        onSubmit={expense => {
-          props.dispatch(editExpense(props.match.params.id, expense));
-          props.history.push('/');
-          console.log('updated', expense);
-        }}
-      />
-      <button
-        onClick={() => {
-          props.dispatch(removeExpense({ id: props.expense.id }));
-          props.history.push('/');
-        }}
-      >
-        Remove
-      </button>
-    </div>
-  );
-};
+class EditExpensePage extends Component {
+  onSubmit = expense => {
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push('/');
+  };
+  onRemove = () => {
+    this.props.startRemoveExpense({ id: this.props.expense.id });
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        Editing the expense with id of {this.props.match.params.id}
+        <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+        <button onClick={this.onRemove}>Remove</button>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state, props) => {
   return {
@@ -37,4 +32,31 @@ const mapStateToProps = (state, props) => {
   };
 };
 
-export default connect(mapStateToProps)(EditExpensePage);
+export default connect(mapStateToProps, { editExpense, startRemoveExpense })(
+  EditExpensePage
+);
+
+// const EditExpensePage = props => {
+//   console.log(props);
+//   return (
+//     <div>
+//       Editing the expense with id of {props.match.params.id}
+//       <ExpenseForm
+//         expense={props.expense}
+//         onSubmit={expense => {
+//           props.dispatch(editExpense(props.match.params.id, expense));
+//           props.history.push('/');
+//           console.log('updated', expense);
+//         }}
+//       />
+//       <button
+//         onClick={() => {
+//           props.dispatch(removeExpense({ id: props.expense.id }));
+//           props.history.push('/');
+//         }}
+//       >
+//         Remove
+//       </button>
+//     </div>
+//   );
+// };
